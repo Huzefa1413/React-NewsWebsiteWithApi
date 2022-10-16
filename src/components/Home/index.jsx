18f924249b4eb2a23499fbb46a50bc5d10555604
Huzefa1413 <79566904+Huzefa1413@users.noreply.github.com>
2022-10-16 13:26:02 +0500

    Navbar Added

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,24 +1,63 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Post from "../../components/Post";
+import "./index.css";
+
 function Home() {
   const [post, setPost] = useState([]);
   useEffect(() => {
     axios
       .get(
-        "https://newsapi.org/v2/everything?q=World&from=2022-10-15&sortBy=popularity&apiKey=299b4698317642c5a2255c1f031f0e1b"
+        `https://newsapi.org/v2/everything?q=${search}&from=2022-10-15&sortBy=popularity&apiKey=299b4698317642c5a2255c1f031f0e1b`
       )
       .then((response) => {
+        console.log(response.data.articles);
+
         setPost(response.data.articles);
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
+  const [search, setSearch] = useState("World");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    axios
+      .get(
+        `https://newsapi.org/v2/everything?q=${search}&from=2022-10-15&sortBy=popularity&apiKey=299b4698317642c5a2255c1f031f0e1b`
+      )
+      .then((response) => {
+        console.log(response.data.articles);
+        setPost(response.data.articles);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <>
-      <h1 style={{ textAlign: "center", margin: "20px" }}>Robotsy News</h1>
-      <div style={{ display: "flex", flexWrap: "wrap" }}>
+      <div className="navbar">
+        <div className="logo">
+          <h1>Robotsy News</h1>
+        </div>
+        <div className="search">
+          <form onSubmit={submitHandler}>
+            <input
+              type="text"
+              placeholder="Search Robotsy News"
+              onChange={(e) => {
+                setSearch(e.target.value);
+              }}
+            />
+            <button type="submit">Search</button>
+          </form>
+        </div>
+      </div>
+
+      <div className="newspostsbody">
         {post.map((eachPost, i) => (
           <Post
             author={eachPost.author}
